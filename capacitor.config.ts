3eac2135c182f038408c7ae8c992c696680c85e5
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2023-09-15 16:58:08 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-loja',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
